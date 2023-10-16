c18a9c69bfcfd5ce1fa8ad4f6fa1adc51aedf180
Alexander Strack <74077030+constracktor@users.noreply.github.com>
2023-10-16 10:31:26 +0200

    prepare gcc-config.sh for rostam gcc

--- gcc-config.sh
+++ gcc-config.sh
@@ -1,5 +1,8 @@
-export CC=gcc${CC_VERSION}
-export CXX=g++${CC_VERSION}
+#export CC=gcc${CC_VERSION}
+#export CXX=g++${CC_VERSION}
+export CC=/opt/apps/gcc/${CC_VERSION}/bin/gcc
+export CXX=/opt/apps/gcc/${CC_VERSION}/bin/g++
+
 if [ -z "$HPX_USE_CC_COMPILER" ]
 then
     export CC=${INSTALL_ROOT}/gcc/bin/gcc
