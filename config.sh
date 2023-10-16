c1f276ac80d2f335e7398c6a7b58f75b98e5b370
Alexander Strack <74077030+constracktor@users.noreply.github.com>
2023-10-16 10:20:07 +0200

    Update config.sh to latest versions

--- config.sh
+++ config.sh
@@ -7,42 +7,42 @@ export SOURCE_ROOT=${HPXSC_ROOT}/../src
 # Package Configuration
 ################################################################################
 # CMake
-export CMAKE_VERSION=3.19.5
+export CMAKE_VERSION=3.28.0
 
 # GCC
-export GCC_VERSION=10.3.0
+#export GCC_VERSION=10.3.0
 # specific version of system GCC
-#export CC_VERSION=-9
+export CC_VERSION=-12
 
 # clang
-export CLANG_VERSION=release/12.x
+#export CLANG_VERSION=release/12.x
 # specific version of system clang
-#export CC_CLANG_VERSION=-10
+export CC_CLANG_VERSION=-17
 
 # OpenMPI
-export OPENMPI_VERSION=4.1.5
+export OPENMPI_VERSION=4.1.6
 
 # MKL version 
 export MKL_VERSION=2023.0.0
 
 # Boost
-export BOOST_VERSION=1.75.0
+export BOOST_VERSION=1.83.0
 export BOOST_ROOT=${INSTALL_ROOT}/boost
 export BOOST_BUILD_TYPE=$(echo ${BUILD_TYPE/%WithDebInfo/ease} | tr '[:upper:]' '[:lower:]')
 
 # jemalloc
-export JEMALLOC_VERSION=5.2.1
+export JEMALLOC_VERSION=5.3.0
 
 # hwloc
-export HWLOC_VERSION=2.9.1
+export HWLOC_VERSION=2.9.3
 
 
 # CUDA
-export CUDA_VERSION=11.0.3
+#export CUDA_VERSION=11.0.3
 #export CUDA_SM=sm_80
 
 # Kokkos
-export CUDA_ARCH=VOLTA70
+#export CUDA_ARCH=VOLTA70
 #export KOKKOS_CONFIG=" -DKokkos_ARCH_HSW=ON  -DKokkos_ARCH_AMPERE80=ON "
 #export KOKKOS_VERSION=d1e00352fd6262fd8d08225eb7086793432db35f
 #export HPX_KOKKOS_VERSION=0.2.0
